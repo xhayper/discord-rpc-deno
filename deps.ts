77851899ea58852a8fbbfcbe1b7ed90ba30ca971
xhayper <46065349+xhayper@users.noreply.github.com>
2022-10-03 12:21:07 +0700

    chore: import order

--- deps.ts
+++ deps.ts
@@ -2,10 +2,10 @@ export { grantOrThrow } from "https://deno.land/std@0.158.0/permissions/mod.ts";
 export { EventEmitter } from "https://deno.land/std@0.158.0/node/events.ts"; // TODO: Stop using node!
 export { Buffer } from "https://deno.land/std@0.158.0/node/buffer.ts"; // TODO: Stop using node!
 
-export * as net from "https://deno.land/std@0.158.0/node/net.ts"; // TODO: Stop using node!
 export * as path from "https://deno.land/std@0.158.0/path/mod.ts";
-export * as fs from "https://deno.land/std@0.158.0/fs/mod.ts";
+export * as net from "https://deno.land/std@0.158.0/node/net.ts"; // TODO: Stop using node!
 export * as log from "https://deno.land/std@0.158.0/log/mod.ts";
+export * as fs from "https://deno.land/std@0.158.0/fs/mod.ts";
 
 export type {
   ActivityType,
