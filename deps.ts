7560df50e4fdc29caccc547a46b717a72843e698
xhayper <46065349+xhayper@users.noreply.github.com>
2022-10-22 15:32:52 +0700

    chore: dependencies update

--- deps.ts
+++ deps.ts
@@ -1,10 +1,10 @@
-export { grantOrThrow } from "https://deno.land/std@0.159.0/permissions/mod.ts";
-export { EventEmitter } from "https://deno.land/std@0.159.0/node/events.ts"; // TODO: Stop using node!
-export { Buffer } from "https://deno.land/std@0.159.0/node/buffer.ts"; // TODO: Stop using node!
+export { grantOrThrow } from "https://deno.land/std@0.160.0/permissions/mod.ts";
+export { EventEmitter } from "https://deno.land/std@0.160.0/node/events.ts"; // TODO: Stop using node!
+export { Buffer } from "https://deno.land/std@0.160.0/node/buffer.ts"; // TODO: Stop using node!
 
-export * as path from "https://deno.land/std@0.159.0/path/mod.ts";
-export * as net from "https://deno.land/std@0.159.0/node/net.ts"; // TODO: Stop using node!
-export * as log from "https://deno.land/std@0.159.0/log/mod.ts";
+export * as path from "https://deno.land/std@0.160.0/path/mod.ts";
+export * as net from "https://deno.land/std@0.160.0/node/net.ts"; // TODO: Stop using node!
+export * as log from "https://deno.land/std@0.160.0/log/mod.ts";
 
 export type {
   ActivityType,
