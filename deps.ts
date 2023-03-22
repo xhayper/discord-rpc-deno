e8292d22504ab618b6d82fc554f3ac5a77315355
xhayper <46065349+xhayper@users.noreply.github.com>
2023-03-22 12:59:15 +0700

    chore: dependencies update

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
-export { grantOrThrow } from "https://deno.land/std@0.177.0/permissions/mod.ts";
-export { EventEmitter } from "https://deno.land/std@0.177.0/node/events.ts"; // TODO: Stop using node!
-export { Buffer } from "https://deno.land/std@0.177.0/node/buffer.ts"; // TODO: Stop using node!
+export { grantOrThrow } from "https://deno.land/std@0.180.0/permissions/mod.ts";
+export { EventEmitter } from "https://deno.land/std@0.180.0/node/events.ts"; // TODO: Stop using node!
+export { Buffer } from "https://deno.land/std@0.180.0/node/buffer.ts"; // TODO: Stop using node!
 
-export * as path from "https://deno.land/std@0.177.0/path/mod.ts";
-export * as net from "https://deno.land/std@0.177.0/node/net.ts"; // TODO: Stop using node!
+export * as path from "https://deno.land/std@0.180.0/path/mod.ts";
+export * as net from "https://deno.land/std@0.180.0/node/net.ts"; // TODO: Stop using node!
 
 export type {
   ActivityType,
@@ -19,4 +19,4 @@ export type {
   PresenceUpdateStatus,
   UserFlags,
   UserPremiumType,
-} from "https://deno.land/x/discord_api_types@0.37.30/v10.ts";
+} from "https://deno.land/x/discord_api_types@0.37.36/v10.ts";
