72d9e99ca7e3b2981677fcfbdda6d68d79ac6706
xhayper <46065349+xhayper@users.noreply.github.com>
2023-11-14 11:13:08 +0700

    feat: bump version

--- examples/simple_status.ts
+++ examples/simple_status.ts
@@ -1,4 +1,4 @@
-import { Client } from "https://deno.land/x/discord_rpc_deno@1.0.24/mod.ts";
+import { Client } from "https://deno.land/x/discord_rpc_deno@1.0.25/mod.ts";
 
 const client = new Client({
   clientId: "123456789012345678",
